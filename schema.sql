6b42432fd83872f2d248ef46ffc46f06170da4f4
matronae <mykaomas@gmail.com>
2024-02-08 20:30:50 -0600

    added role database

--- schema.sql
+++ schema.sql
@@ -5,3 +5,13 @@ CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
 );
+
+-- role database
+USE role;
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+)
